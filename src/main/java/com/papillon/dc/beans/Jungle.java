d57dbf3d2fd568c9cbf333f76e58fd5361b9ff39
papillon <devopreet.ch@gmail.com>
2017-05-15 10:58:59 -0500

    *maps initialization

--- src/main/java/com/papillon/dc/beans/Jungle.java
+++ src/main/java/com/papillon/dc/beans/Jungle.java
@@ -1,13 +1,15 @@
 package com.papillon.dc.beans;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by papillon on 5/15/2017.
  */
 public class Jungle {
 
-    private Animal largest;
+    /*private Animal largest;
     private List<Animal> animals;
 
     public Animal getLargest() {
@@ -32,6 +34,39 @@ public class Jungle {
         sb.append(largest+"\n");
         for(Animal animal : animals){
             sb.append(animal+"\n");
+        }
+        return sb.toString();
+    }*/
+
+    //initializing beans containing maps
+    private Map<String,String> foods = new HashMap<>();
+    private Map<String,Animal> animals = new HashMap<>();
+
+    public Map<String, Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(Map<String, Animal> animals) {
+        this.animals = animals;
+    }
+
+    public Map<String, String> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(Map<String, String> foods) {
+        this.foods = foods;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<String,String> map : foods.entrySet()){
+            sb.append(map.getKey()+" "+map.getValue()+"\n");
+        }
+        sb.append("\n");
+        for(Map.Entry<String,Animal> map : animals.entrySet()){
+            sb.append(map.getKey()+" "+map.getValue()+"\n");
         }
         return sb.toString();
     }
